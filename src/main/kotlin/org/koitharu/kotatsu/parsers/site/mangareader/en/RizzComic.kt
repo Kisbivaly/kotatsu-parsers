62c8305687078ec78ee0a2ede0f1808245b31dcf
bivaly <71719024+Kisbivaly@users.noreply.github.com>
2025-02-01 21:21:22 +0100

    Update RizzComic.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/en/RizzComic.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/en/RizzComic.kt
@@ -17,7 +17,7 @@ import java.util.*
 
 @MangaSourceParser("RIZZCOMIC", "RizzComic", "en")
 internal class RizzComic(context: MangaLoaderContext) :
-	MangaReaderParser(context, MangaParserSource.RIZZCOMIC, "realmoasis.com", pageSize = 50, searchPageSize = 20) {
+	MangaReaderParser(context, MangaParserSource.RIZZCOMIC, "realmoasis.com/6810173843280087", pageSize = 50, searchPageSize = 20) {
 
 	override val datePattern = "dd MMM yyyy"
 	override val listUrl = "/home"
